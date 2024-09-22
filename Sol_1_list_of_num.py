0c2c63d3a6e3a40d8225cb822d1e8c6a858c68ed
AMOGH441 <amoghshukla441@gmail.com>
2024-09-22 14:30:40 +0530

    question01

--- Sol_1_list_of_num.py
+++ Sol_1_list_of_num.py
@@ -1,13 +1,15 @@
-num = int(input("How many numbers are needed to add in the list of number?: "))
-list1 = []
-for i in range(1,num+1):
-    number_add = int(input(f"give the {i} number to add the list: "))
-    list1.append(number_add)
-print(list1)
+def accept_numbers():
+    num = int(input("How many numbers are needed to add in the list of number?: "))
+    list1 = []
+    for i in range(1,num+1):
+        number_add = int(input(f"give the {i} number to add the list: "))
+        list1.append(number_add)
+    print(list1)
+    evenlist =[]
+    for i in  list1:
+        if i %2 ==0:
+            evenlist.append(i)
+    return evenlist  
 
-evenlist =[]
-for i in  list1:
-    if i %2 ==0:
-        evenlist.append(i)
-    
-print(f"all the even number from the input list{evenlist}")
+accept_numbers
+print(f"list of all even numbers:  {accept_numbers()}" )
